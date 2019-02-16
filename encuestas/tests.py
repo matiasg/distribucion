9d4122442065781117eb3b1050dad69c3a3ca2e5
Matías Graña <matias.alejo@gmail.com>
2019-02-16 08:03:50 -0300

    arreglo tests

--- encuestas/tests.py
+++ encuestas/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test.utils import setup_test_environment
 
-from materias.models import Docente, Cargos, Materia, TipoMateria
+from materias.models import Docente, Cargos, Materia, TipoMateria, Cuatrimestres
 from .views import checkear_y_salvar
 
 
@@ -16,24 +16,24 @@ class TestEncuesta(TestCase):
     def test_sin_docente(self):
         datos = {}
         with self.assertRaises(KeyError):
-            checkear_y_salvar(datos)
+            checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
     def test_docente_no_existe(self):
         docente_inexistente_id = str(int(self.docente_id) + 1)
         datos = {'docente': docente_inexistente_id}
         with self.assertRaises(Docente.DoesNotExist):
-            checkear_y_salvar(datos)
+            checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
     def test_docente_sin_opciones(self):
         datos = {'docente': self.docente_id}
         with self.assertRaises(KeyError):
-            checkear_y_salvar(datos)
+            checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
     def test_docente_y_opciones_vacias(self):
         datos = {'docente': self.docente_id}
         for opcion in range(1, 6):
             datos['opcion{}'.format(opcion)] = ''
             datos['peso{}'.format(opcion)] = str(opcion)
-        checkear_y_salvar(datos)
+        checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
 
