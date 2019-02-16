4625585109ca436c08aa0b6d844726c863002b94
Matías Graña <matias.alejo@gmail.com>
2019-02-16 08:45:43 -0300

    mejores tests

--- encuestas/tests.py
+++ encuestas/tests.py
@@ -1,17 +1,19 @@
 from django.test import TestCase
 from django.test.utils import setup_test_environment
 
-from materias.models import Docente, Cargos, Materia, TipoMateria, Cuatrimestres
+from materias.models import Docente, Cargos, Materia, Turno, TurnoTipos, TipoMateria, Cuatrimestres
+from .models import PreferenciasDocente
 from .views import checkear_y_salvar
 
 
 class TestEncuesta(TestCase):
 
     def setUp(self):
-        Docente.objects.create(nombre='juan', email='mail@nada.org', telefono='1234', cargo=Cargos.JTP, cargas=1)
-        Materia.objects.create(nombre='epistemologia', obligatoriedad=TipoMateria.B)
-        self.docente_id = str(Docente.objects.first().id)
-        self.materia_id = str(Materia.objects.first().id)
+        self.docente = Docente.objects.create(nombre='juan', email='mail@nada.org',
+                                              telefono='1234', cargo=Cargos.JTP, cargas=1)
+        self.materia = Materia.objects.create(nombre='epistemologia', obligatoriedad=TipoMateria.B)
+        self.turno = Turno.objects.create(materia=self.materia, anno=2100, cuatrimestre=Cuatrimestres.P.name,
+                                          numero=1, tipo=TurnoTipos.T.name, necesidades='1,0,0')
 
     def test_sin_docente(self):
         datos = {}
@@ -19,21 +21,51 @@ class TestEncuesta(TestCase):
             checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
     def test_docente_no_existe(self):
-        docente_inexistente_id = str(int(self.docente_id) + 1)
+        docente_inexistente_id = str(self.docente.id + 1)
         datos = {'docente': docente_inexistente_id}
         with self.assertRaises(Docente.DoesNotExist):
             checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
     def test_docente_sin_opciones(self):
-        datos = {'docente': self.docente_id}
+        datos = {'docente': self.docente.id}
         with self.assertRaises(KeyError):
             checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
 
+    def test_turno_no_existe(self):
+        datos = {'docente': self.docente.id}
+        for opcion in range(1, 6):
+            datos['opcion{}'.format(opcion)] = str(self.turno.id + 1)
+            datos['peso{}'.format(opcion)] = str(opcion)
+        with self.assertRaises(Turno.DoesNotExist):
+            checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
+
     def test_docente_y_opciones_vacias(self):
-        datos = {'docente': self.docente_id}
+        datos = {'docente': self.docente.id}
         for opcion in range(1, 6):
             datos['opcion{}'.format(opcion)] = ''
             datos['peso{}'.format(opcion)] = str(opcion)
         checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
+        self.assertEqual(len(PreferenciasDocente.objects.all()), 0)
 
+    def test_docente_y_opciones_con_sentido(self):
+        datos = {'docente': self.docente.id}
+        for opcion in range(1, 6):
+            turno = Turno.objects.create(materia=self.materia, anno=2100, cuatrimestre=Cuatrimestres.P.name,
+                                         numero=1, tipo=TurnoTipos.T.name, necesidades='1,0,0')
+            datos['opcion{}'.format(opcion)] = turno.id
+            datos['peso{}'.format(opcion)] = str(opcion)
+        checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
+        self.assertEqual(len(PreferenciasDocente.objects.all()), 5)
 
+    def test_algunas_opciones_vacias(self):
+        datos = {'docente': self.docente.id}
+        for opcion in range(1, 3):
+            turno = Turno.objects.create(materia=self.materia, anno=2100, cuatrimestre=Cuatrimestres.P.name,
+                                         numero=1, tipo=TurnoTipos.T.name, necesidades='1,0,0')
+            datos['opcion{}'.format(opcion)] = turno.id
+            datos['peso{}'.format(opcion)] = str(opcion)
+        for opcion in range(3, 6):
+            datos['opcion{}'.format(opcion)] = ''
+            datos['peso{}'.format(opcion)] = str(opcion)
+        checkear_y_salvar(datos, 2100, Cuatrimestres.P.name)
+        self.assertEqual(len(PreferenciasDocente.objects.all()), 2)
