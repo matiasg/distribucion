458cedfb339a5f466b30e96fec97773a84258af5
Matías Graña <matias.alejo@gmail.com>
2019-03-07 01:53:41 -0300

    views: dborrador.distribuir

--- dborrador/views.py
+++ dborrador/views.py
@@ -1,5 +1,4 @@
 import logging
-import logging.config
 
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
@@ -9,9 +8,12 @@ from django.utils import timezone
 from .models import Preferencia, Asignacion
 from materias.models import Turno, Cuatrimestres, choice_enum
 from encuestas.models import PreferenciasDocente
+from encuestas.views import Mapeos
+
+from allocation import allocating
+
 
 logger = logging.getLogger(__name__)
-
 
 
 def index(request):
@@ -40,4 +42,40 @@ def preparar(request):
                 'annos': [anno_actual, anno_actual + 1],
                 'cuatrimestres': [c for c in Cuatrimestres]}
         return render(request, 'dborrador/elegir_ac.html', context)
+
+
+def distribuir(request):
+    try:
+        anno = request.POST['anno']
+        cuatrimestre = request.POST['cuatrimestre']
+        tipo = request.POST['tipo']
+        logger.warning('comienzo una distribución para docentes tipo %s, cuatrimestre %s, año %s',
+                    tipo, cuatrimestre, anno)
+
+        docentes = Mapeos.docentes(tipo)
+        turnos = Mapeos.encuesta_tipo_turno(tipo)
+        preferencias = Preferencia.objects.all()
+
+        sources = {d.id: d.cargas for d in docentes}
+
+        necesidad_indice = 'PJA'.index(tipo)
+        targets = {t.id: int(t.necesidades.split(',')[necesidad_indice]) for t in turnos}
+
+        pesos = [{'from': p.preferencia.docente.id, 'to': p.preferencia.turno.id, 'weight': p.preferencia.peso}
+                 for p in preferencias]
+        wmap = allocating.WeightedMap(pesos)
+
+        allocator = allocating.Allocator(sources, wmap, targets)
+        distribucion = allocator.get_best()
+        logger.info('te conseguí esto: %s', distribucion)
+
+        return HttpResponseRedirect(reverse('dborrador:index'))
+
+    except KeyError:
+        anno_actual = timezone.now().year
+        context = {
+                'annos': [anno_actual, anno_actual + 1],
+                'cuatrimestres': [c for c in Cuatrimestres],
+                'tipos': ['P', 'J', 'A']}
+        return render(request, 'dborrador/distribuir.html', context)
 
