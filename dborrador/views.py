984324c19ea1b847ee5985ce71cd400e24c881ef
Matías Graña <matias.alejo@gmail.com>
2019-03-08 01:30:21 -0300

    paso resultado de allocation a asignaciones y lo muestro

--- dborrador/views.py
+++ dborrador/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from .models import Preferencia, Asignacion
-from materias.models import Turno, Cuatrimestres, choice_enum
+from materias.models import Turno, Docente, Materia, Cuatrimestres, TipoMateria, choice_enum
 from encuestas.models import PreferenciasDocente
 from encuestas.views import Mapeos
 
@@ -49,12 +49,25 @@ def distribuir(request):
         anno = request.POST['anno']
         cuatrimestre = request.POST['cuatrimestre']
         tipo = request.POST['tipo']
+        intento = 1
+
+    except KeyError:
+        anno_actual = timezone.now().year
+        context = {
+                'annos': [anno_actual, anno_actual + 1],
+                'cuatrimestres': [c for c in Cuatrimestres],
+                'tipos': ['P', 'J', 'A']}
+        return render(request, 'dborrador/distribuir.html', context)
+
+    else:
         logger.warning('comienzo una distribución para docentes tipo %s, cuatrimestre %s, año %s',
                     tipo, cuatrimestre, anno)
 
         docentes = Mapeos.docentes(tipo)
         turnos = Mapeos.encuesta_tipo_turno(tipo)
         preferencias = Preferencia.objects.all()
+
+        logger.info('%d docentes, %d turnos, %d preferencias', len(docentes), len(turnos), len(preferencias))
 
         sources = {str(d.id): d.cargas for d in docentes}
 
@@ -67,15 +80,39 @@ def distribuir(request):
 
         allocator = allocating.Allocator(sources, wmap, targets)
         distribucion = allocator.get_best()
-        logger.info('te conseguí esto: %s', distribucion)
+        logger.info('distribución obtenida (con ids): %s', distribucion)
 
-        return HttpResponseRedirect(reverse('dborrador:index'))
+        for docente_id, turno_id in distribucion:
+            if turno_id is None:
+                continue
+            if docente_id is None:
+                continue
 
-    except KeyError:
-        anno_actual = timezone.now().year
-        context = {
-                'annos': [anno_actual, anno_actual + 1],
-                'cuatrimestres': [c for c in Cuatrimestres],
-                'tipos': ['P', 'J', 'A']}
-        return render(request, 'dborrador/distribuir.html', context)
+            docente = Docente.objects.get(pk=int(docente_id))
+            turno = Turno.objects.get(pk=turno_id)
+            asignacion, _ = Asignacion.objects.get_or_create(
+                                        intento=intento, docente=docente, turno=turno)
 
+        context = {'materias': filtra_materias(anno=anno, cuatrimestre=cuatrimestre)}
+        return render(request, 'dborrador/distribucion.html', context)
+
+
+def filtra_materias(**kwargs):
+    turnos = Turno.objects.filter(**kwargs)
+    tipo_dict = {TipoMateria.B.name: 'Obligatorias',
+                 TipoMateria.R.name: 'Optativas regulares',
+                 TipoMateria.N.name: 'Optativas no regulares'}
+
+    materias = []
+    for tipo, tipo_largo in tipo_dict.items():
+        tmaterias = Materia.objects.filter(obligatoriedad=tipo)
+        materias_turnos = [
+                (materia, Turno.objects.filter(materia=materia, **kwargs))
+                for materia in tmaterias
+                ]
+        for materia, turnos in materias_turnos:
+            for turno in turnos:
+                turno.docentes_asignados = [a.docente for a in turno.asignacion_set.all()]
+        materias.append((tipo_largo, materias_turnos))
+
+    return materias
