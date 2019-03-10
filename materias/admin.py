e29cfe6bf51a1fe9a94abf8a2e8b98050bd71f64
Matías Graña <matias.alejo@gmail.com>
2019-03-10 11:11:06 -0300

    agrego CuatrimestreDocente al admin

--- materias/admin.py
+++ materias/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Materia, Turno, Horario, Docente, Carga
+from .models import Materia, Turno, Horario, Docente, Carga, CuatrimestreDocente
 
-admin.site.register([Materia, Turno, Horario, Docente, Carga])
+admin.site.register([Materia, Turno, Horario, Docente, Carga, CuatrimestreDocente])
