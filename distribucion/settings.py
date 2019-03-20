7cdc35c7c42d7518deb259c149a0f6935dd9c308
Matías Graña <matias.alejo@gmail.com>
2019-03-20 08:38:48 -0300

    sqlite3 -> postgresql

--- distribucion/settings.py
+++ distribucion/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = 'distribucion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
