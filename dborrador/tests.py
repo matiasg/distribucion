12141e872ed4163c3c557a05d25092f1e4bf076b
Matías Graña <matias.alejo@gmail.com>
2019-04-07 23:15:45 -0300

    arreglo test viejo de dborrador

--- dborrador/tests.py
+++ dborrador/tests.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 import re
 
 from dborrador.models import Preferencia, Asignacion
-from materias.models import Docente, Materia, Turno, Cuatrimestres, Cargos, TipoTurno, TipoMateria
+from materias.models import Docente, Materia, Turno, Cuatrimestres, Cargos, CargoDedicacion, TipoTurno, TipoMateria
 from encuestas.models import PreferenciasDocente
 
 class TestPreparar(TestCase):
@@ -28,7 +28,8 @@ class TestPreparar(TestCase):
 
     def test_copia_preferencias(self):
         docente = Docente.objects.create(nombre='juan', email='mail@nada.org',
-                                         telefono='1234', cargo=Cargos.Tit.name)
+                                         telefono='1234',
+                                         cargos=[CargoDedicacion.TitSim.name])
         materia = Materia.objects.create(nombre='epistemologia', obligatoriedad=TipoMateria.B.name)
         turno1 = Turno.objects.create(materia=materia, anno=2100, cuatrimestre=Cuatrimestres.P.name,
                                       numero=1, tipo=TipoTurno.A.name,
@@ -62,5 +63,5 @@ class TestPaginaPrincipal(TestCase):
                         'La página principal no contiene un link a /dborrador/preparar')
         self.assertTrue(re.search('<a href=.distribuir.>', response.content.decode()),
                         'La página principal no contiene un link a /dborrador/distribuir')
-        self.assertTrue(re.search('<a href=.distribucion/\d+/[PSV]/[PJA12]/\d+', response.content.decode()),
-                        'La página principal no contiene un link a /dborrador/distribucion/... completo')
+        self.assertTrue(re.search(r'<form action="/dborrador/" method="post">', response.content.decode()),
+                        'La página principal no contiene un botón de post a /dborrador/')
