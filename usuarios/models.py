2fe2388130efd5dba57c6ae801483f8425dd9c92
Matías Graña <matias.alejo@gmail.com>
2019-04-20 15:38:02 -0300

    nueva app usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,7 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+from dborrador.models import Preferencia, Asignacion
+
 
 class Usuario(AbstractUser):
 
-    puede_distribuir = models.BooleanField('puede distribuir',
-                                           default=False)
+    pass
+
+class Grupo(Group):
+
+    pass
+
+
+def crear_grupos_fijos():
+    permisos_dborrador = Permission.objects.filter(content_type__app_label='dborrador')
+    permisos_materias = Permission.objects.filter(content_type__app_label='materias')
+    permiso_ver_encuestas = Permission.objects.filter(content_type__app_label='encuestas',
+                                                      codename__startswith='view')
+
+    secretaries, _ = Grupo.objects.get_or_create(name='Secretaries')
+    secretaries.permissions.add(*permisos_materias)
+
+    distribuidores, _ = Grupo.objects.get_or_create(name='Distribuidores')
+    distribuidores.permissions.add(*permisos_dborrador, *permiso_ver_encuestas)
