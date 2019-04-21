f8f14d50070d703e5723676ff58a10600997214a
Matías Graña <matias.alejo@gmail.com>
2019-04-21 01:56:53 -0300

    SimpleHistoryAdmin en materias

--- materias/admin.py
+++ materias/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from .models import Materia, Turno, Horario, Docente, Carga, CuatrimestreDocente
 
-admin.site.register([Materia, Turno, Horario, Docente, Carga, CuatrimestreDocente])
+admin.site.register([Materia, Horario, Docente, CuatrimestreDocente])
+admin.site.register([Carga, Turno], SimpleHistoryAdmin)
