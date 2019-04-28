cfab23806b3aa21d9ca94730365bc2bf9c2061fd
Matías Graña <matias.alejo@gmail.com>
2019-04-27 21:58:42 -0300

    quito Group del admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+import django.contrib.auth.admin
 
 from .models import Usuario, Grupo
 
+admin.site.unregister([Group])
 admin.site.register([Usuario, Grupo])
