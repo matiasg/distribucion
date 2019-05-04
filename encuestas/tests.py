6f338bd905680537684fc7573cd71ab47e3a5bd0
Matías Graña <matias.alejo@gmail.com>
2019-05-04 18:21:47 -0300

    agrego test

--- encuestas/tests.py
+++ encuestas/tests.py
@@ -1,10 +1,13 @@
-from django.test import TestCase, tag
+from django.test import TestCase, Client
 from django.test.utils import setup_test_environment
 from django.urls import reverse
 from django.forms import ValidationError
 
+import re
+
 from materias.models import (Docente, Cargos, Materia, Turno, TipoTurno, TipoMateria,
                              CargoDedicacion, Cuatrimestres)
+from materias.misc import TipoDocentes
 from .models import PreferenciasDocente
 from .views import checkear_y_salvar
 
@@ -20,6 +23,10 @@ class TestEncuesta(TestCase):
         self.turno = Turno.objects.create(materia=self.materia, anno=self.anno, cuatrimestre=Cuatrimestres.P.name,
                                           numero=1, tipo=TipoTurno.A.name,
                                           necesidad_prof=1, necesidad_jtp=0, necesidad_ay1=0, necesidad_ay2=0)
+        self.turno_dificil = Turno.objects.create(materia=self.materia, anno=self.anno, cuatrimestre=Cuatrimestres.P.name,
+                                                  numero=2, tipo=TipoTurno.T.name,
+                                                  necesidad_prof=1, necesidad_jtp=0, necesidad_ay1=0, necesidad_ay2=0,
+                                                  dificil_de_cubrir=True)
 
     def test_sin_docente(self):
         datos = {}
@@ -111,3 +118,12 @@ class TestEncuesta(TestCase):
         datos['opcion2'] = turnos[0].id
         with self.assertRaises(ValidationError):
             checkear_y_salvar(datos)
+
+    def test_encuesta_distingue_turnos_dificiles(self):
+        '''Chequeamos que el turno dificil aparece 5 veces en la encuesta y el facil 3'''
+        c = Client()
+        response = c.get(f'/encuestas/encuesta/{self.anno}/{Cuatrimestres.P.name}/{TipoDocentes.P.name}')
+        opcion_reg_facil = re.compile('^.*<option value.*Teórico-Práctica 1', re.MULTILINE)
+        opcion_reg_dificil = re.compile('^.*<option value.*Teórica 2', re.MULTILINE)
+        self.assertEqual(len(opcion_reg_facil.findall(response.content.decode())), 3)
+        self.assertEqual(len(opcion_reg_dificil.findall(response.content.decode())), 5)
