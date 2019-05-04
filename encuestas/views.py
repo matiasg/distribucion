cdfcf4e477603ef9c53b511549a32c68342648be
Matías Graña <matias.alejo@gmail.com>
2019-05-04 18:21:40 -0300

    agrego opciones fáciles y difíciles en el contexto

--- encuestas/views.py
+++ encuestas/views.py
@@ -62,7 +62,9 @@ def encuesta(request, anno, cuatrimestre, tipo_docente):
     turnos = Mapeos.encuesta_tipo_turno(tipo).filter(anno=anno, cuatrimestre=cuatrimestre)
     turnos = sorted(turnos, key=lambda t: t.materia.nombre)
     docentes = sorted(Mapeos.docentes_de_tipo(tipo), key=lambda d: d.nombre)
-    context = {'turnos': turnos,
+    opciones = [(i, i > 2) for i in range(1, 6)]  # las opciones 1 y 2 tienen que ser de las difíciles
+    context = {'opciones': opciones,
+               'turnos': turnos,
                'docentes': docentes,
                'anno': anno,
                'cuatrimestre': cuatrimestre,
