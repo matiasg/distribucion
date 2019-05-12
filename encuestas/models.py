c489c50ebe882d6d7a5b81dc8bccc34358a8f5ae
Matías Graña <matias.alejo@gmail.com>
2019-05-12 16:12:03 -0300

    nueva clase OtrosDatos que van en la encuesta

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
+from django.core.validators import MaxValueValidator, RegexValidator
 
 from materias.models import Turno, Docente, Cuatrimestres, Cargos, choice_enum
 
@@ -13,3 +14,15 @@ class PreferenciasDocente(models.Model):
 
     def __str__(self):
         return f'{self.docente} -- {self.peso} -> {self.turno}'
+
+telefono_validator = RegexValidator(regex=r'^\+?[0-9 -]{9,15}$',
+                                    message=("El teléfono debe contener +, -, números, espacios "
+                                             "y tener longitud de teléfono"))
+
+class OtrosDatos(models.Model):
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE)
+    fecha_encuesta = models.DateTimeField()
+    comentario = models.TextField()
+    cargas = models.PositiveIntegerField(validators=[MaxValueValidator(3)])
+    email = models.EmailField()
+    telefono = models.CharField(validators=[telefono_validator], max_length=17, blank=True)
