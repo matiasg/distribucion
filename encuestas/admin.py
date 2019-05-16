d94fcdc550ec15ea22328e7b752d3386361b5c32
Matías Graña <matias.alejo@gmail.com>
2019-05-16 08:49:49 -0300

    agrego OtrosDatos al admin

--- encuestas/admin.py
+++ encuestas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 
-from .models import PreferenciasDocente
+from .models import PreferenciasDocente, OtrosDatos
 
 admin.site.register([PreferenciasDocente], SimpleHistoryAdmin)
+admin.site.register([OtrosDatos])
