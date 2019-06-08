28d622bfbc519adda718c12e57d2f239f6b78be7
Matías Graña <matias.alejo@gmail.com>
2019-06-08 17:26:18 -0300

    borro dos métodos que no se usan más

--- dborrador/misc.py
+++ dborrador/misc.py
@@ -9,30 +9,6 @@ Problemas = namedtuple('Problemas', ['cargas_no_distribuidas', 'necesidades_no_c
 class MapeosDistribucion:
 
     @staticmethod
-    def cargas_a_distribuir(tipo, ac, intento):
-        '''TipoDocentes -> AnnoCuatrimestre -> intento -> {Carga}'''
-        asignaciones_previas = MapeosDistribucion.asignaciones(tipo, ac, intento)
-        docentes_cargas = Mapeos.docentes_y_cargas(tipo, ac)
-        cargas = {c
-                  for d_cargas in docentes_cargas.values()
-                  for c in d_cargas}
-        # TODO: chequear que no hay dos asignaciones con la misma carga
-        cargas_distribuidas = {a.carga for a in asignaciones_previas}
-        return cargas - cargas_distribuidas
-
-    @staticmethod
-    def necesidades_no_cubiertas(tipo, ac, intento):
-        '''TipoDocentes -> AnnoCuatrimestre -> intento -> {Turno: necesidad}.
-        Obs: necesidad puede ser < 0 (y significa una mala distribución)'''
-        asignaciones_previas = MapeosDistribucion.asignaciones(tipo, ac, intento)
-        asignaciones_por_turno = Counter(a.turno for a in asignaciones_previas)
-        turnos = Mapeos.encuesta_tipo_turno(tipo).filter(anno=ac.anno, cuatrimestre=ac.cuatrimestre)
-        necesidades = {}
-        for turno in turnos:
-            necesidades[turno] = Mapeos.necesidades(turno, tipo) - asignaciones_por_turno.get(turno, 0)
-        return necesidades
-
-    @staticmethod
     def cargas_tipo_ge_a_distribuir_en(tipo, ac, intento):
         '''TipoDocentes -> AnnoCuatrimestre -> intento -> [Carga]'''
         todas = Mapeos.cargas_no_asignadas_en(ac)
