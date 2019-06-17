c1bec2f7be02449a2ae8d28d02c299a3643f1d9d
Matías Graña <matias.alejo@gmail.com>
2019-06-16 23:33:21 -0300

    ordeno docentes que piden materias por cargo

--- dborrador/views.py
+++ dborrador/views.py
@@ -147,8 +147,9 @@ def ver_distribucion(request, anno, cuatrimestre, intento_algoritmo, intento_man
     asignaciones_moviles = Distribucion.asignaciones_por_cargo_ocupado(anno_cuat, intento)
     asignaciones_fijas = Distribucion.ya_distribuidas_por_cargo(anno_cuat)
 
-    preferencias = Preferencia.objects.order_by('peso_normalizado')
-    preferencias_por_turno = {turno: preferencias.filter(preferencia__turno=turno).all() for turno in turnos_ac.all()}
+    preferencias = Preferencia.objects.order_by('preferencia__cargo', 'peso_normalizado', 'preferencia__docente__nombre')
+    preferencias_por_turno = {turno: preferencias.filter(preferencia__turno=turno).all()
+                              for turno in turnos_ac.all()}
 
     materias = []
     for obligatoriedad, obligatoriedad_largo in obligatoriedades.items():
@@ -170,7 +171,8 @@ def ver_distribucion(request, anno, cuatrimestre, intento_algoritmo, intento_man
 
     cargas_sin_distribuir = Distribucion.no_distribuidas_por_cargo(anno_cuat)
     cargas_de_asignaciones_moviles = {a.carga for a in Asignacion.validas_en(intento)}
-    cargas_sin_asignar = {tipo: sorted(set(cargas_sin_distribuir[tipo]) - cargas_de_asignaciones_moviles, key=lambda c: c.docente.nombre)
+    cargas_sin_asignar = {tipo: sorted(set(cargas_sin_distribuir[tipo]) - cargas_de_asignaciones_moviles,
+                                       key=lambda c: c.docente.nombre)
                           for tipo in TipoDocentes}
     # agrego preferencias al dict
     cargas_sin_asignar_anotadas = {tipo: [(carga, preferencias.filter(preferencia__docente=carga.docente).all())
