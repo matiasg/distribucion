84554c08f37543515c57048c097620f309042f0a
Matías Graña <matias.alejo@gmail.com>
2019-06-20 18:50:59 -0300

    arreglo tests de usuarios

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -11,25 +11,25 @@ class Usuarios(TestCase):
         self.autorizado = Usuario.objects.create_user(username='autorizado', password='1234')
         permiso = Permission.objects.get(codename='add_asignacion')
         self.autorizado.user_permissions.add(permiso)
-        self.context = (2100, 'P', 'P', 1)
+        self.args = (2100, 'P', 'P', 1, 0)
 
     def test_puede_distribuir_deslogueado(self):
         client = Client()
-        response = client.get(reverse('dborrador:distribuir', args=self.context), follow=True)
+        response = client.get(reverse('dborrador:distribuir', args=self.args), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.redirect_chain[0], ('/admin/login?next=/dborrador/distribuir/2100/P/P/1', 302))
+        self.assertEqual(response.redirect_chain[0], ('/admin/login?next=/dborrador/distribuir/2100/P/P/1/0', 302))
 
     def test_puede_distribuir_logueado_autorizado(self):
         client = Client()
         client.login(username='autorizado', password='1234')
-        response = client.get(reverse('dborrador:distribuir', args=self.context), follow=True)
+        response = client.get(reverse('dborrador:distribuir', args=self.args), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.redirect_chain[0][0], '/dborrador/fijar/2100/P/P/1')
+        self.assertEqual(response.redirect_chain[0][0], '/dborrador/distribucion/2100/P/2/0')
 
     def test_puede_distribuir_logueado_desautorizado(self):
         client = Client()
         client.login(username='desautorizado', password='1234')
-        response = client.get(reverse('dborrador:distribuir', args=self.context), follow=True)
+        response = client.get(reverse('dborrador:distribuir', args=self.args), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.redirect_chain[0], ('/admin/login?next=/dborrador/distribuir/2100/P/P/1', 302))
+        self.assertEqual(response.redirect_chain[0], ('/admin/login?next=/dborrador/distribuir/2100/P/P/1/0', 302))
         self.assertEqual(response.redirect_chain[-1], ('/admin/', 302))
