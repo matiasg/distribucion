86b206bc5586e7fda86e9f8d36c28e9d90c1885d
Matías Graña <matias.alejo@gmail.com>
2019-06-30 18:25:05 -0300

    agrego test de orden a mano por apellido_nombre usando strxfrm

--- materias/tests.py
+++ materias/tests.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 import re
 import datetime
+from locale import strxfrm
 from django.utils import timezone
 
 from materias.models import (Cargos, Carga, Dedicaciones, CargoDedicacion, Docente,
@@ -135,6 +136,9 @@ class TestModels(TestCase):
         a = Docente.objects.create(na_nombre='Áb', na_apellido='A',
                                    telefono='00 0000', email='ab@nada.org', cargos=[CargoDedicacion.TitExc.name])
         docentes = Docente.objects.all()
+        self.assertEquals(list(docentes), [a, b, c])
+
+        docentes = sorted(Docente.objects.all(), key=lambda d: strxfrm(d.apellido_nombre))
         self.assertEquals(list(docentes), [a, b, c])
 
 
