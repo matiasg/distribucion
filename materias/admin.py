8a9bd2369e7148aa49a7b848cfc317326d21830c
Matías Graña <matias.alejo@gmail.com>
2019-07-06 08:35:00 -0300

    mejoro los horarios en el admin para turnos

--- materias/admin.py
+++ materias/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
+from django.db import models
+from django.forms.widgets import TimeInput, Select
 
 from .models import Materia, Turno, Horario, Docente, Carga
 
@@ -8,6 +10,13 @@ from .models import Materia, Turno, Horario, Docente, Carga
 class HorariosInLine(admin.TabularInline):
 
     model = Horario
+    max_num = 3
+    formfield_overrides = {
+        models.TimeField: {'widget': Select(choices=(('', ''),
+                                                     *((f'{hora:02d}:{minutos:02d}:00', f'{hora:02d}:{minutos:02d}')
+                                                       for hora in range(6, 24) for minutos in (0, 30) )
+                                                     ))},
+    }
 
 
 class TurnoAdmin(admin.ModelAdmin):
