c77af75667aaf404458a4b6eac5a20121b7849fd
Matías Graña <matias.alejo@gmail.com>
2019-07-06 22:38:17 -0300

    simplifico la entrada de turnos

--- materias/admin.py
+++ materias/admin.py
@@ -5,23 +5,24 @@ from django.forms.widgets import TimeInput, Select
 
 from .models import Materia, Turno, Horario, Docente, Carga
 
-
+hora_choices = (('', ''),
+                *((f'{hora:02d}:{minutos:02d}:00', f'{hora:02d}:{minutos:02d}')
+                  for hora in range(6, 24) for minutos in (0, 30) )
+                )
 
 class HorariosInLine(admin.TabularInline):
 
     model = Horario
     max_num = 3
     formfield_overrides = {
-        models.TimeField: {'widget': Select(choices=(('', ''),
-                                                     *((f'{hora:02d}:{minutos:02d}:00', f'{hora:02d}:{minutos:02d}')
-                                                       for hora in range(6, 24) for minutos in (0, 30) )
-                                                     ))},
+        models.TimeField: {'widget': Select(choices=hora_choices)},
     }
 
 
 class TurnoAdmin(admin.ModelAdmin):
 
     inlines = [HorariosInLine]
+    fields = ('materia', 'anno', 'cuatrimestre', 'tipo', 'numero')
 
 
 admin.site.register([Materia, Carga, Docente], SimpleHistoryAdmin)
