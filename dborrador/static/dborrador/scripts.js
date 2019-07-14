5afe095d17bc78cada398fe8616d1dc4e8557956
Matías Graña <matias.alejo@gmail.com>
2019-07-14 17:47:10 -0300

    botones que actúan y se parecen a otros.
    Además agrego diálogo de confirmación

--- dborrador/static/dborrador/scripts.js
+++ dborrador/static/dborrador/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $( "#preguntas_distribucion_algoritmo" ).dialog({ autoOpen: false });
     $( "#mandar_a_publicar" ).dialog({ autoOpen: false });
+    $( "#cambiar_docente_dialogo" ).dialog({ autoOpen: false });
 });
 $(document).ready(function(){
     $( ".base_opciones #distribuir" ).click(function() {
@@ -14,5 +15,19 @@ $(document).ready(function(){
     });
     $( ".base_opciones #publicar" ).click(function() {
         $( "#mandar_a_publicar" ).dialog( "open" );
+    });
+    $( "#actuar #cambiar_docente" ).click(function() {
+        $( "#cambiar_docente_dialogo" ).dialog( "open" );
+        $( "#cambiar_docente_dialogo" ).dialog({
+            buttons: [
+                {
+                    text: 'Sí',
+                    click: function() {
+                        $("#cambiar_docente").submit();
+                    },
+                    icons: { primary: 'ui-icon-check' }
+                }
+            ]
+        });
     });
 });
