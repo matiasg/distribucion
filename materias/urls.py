27aedc8c9237565768ab4ede09083ced02b3e2cd
Matías Graña <matias.alejo@gmail.com>
2019-07-22 08:50:58 -0300

    agrego url y muevo al fondo la url de ver horarios

--- materias/urls.py
+++ materias/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
          views.cambiar_una_carga_publicada, name='cambiar_una_carga_publicada'),
     path('administrar_cargas_un_docente/<int:anno>/<str:cuatrimestre>/<int:docente_id>',
          views.administrar_cargas_de_un_docente, name='administrar_cargas_un_docente'),
-    path('<str:anno_cuat>', views.por_anno_y_cuatrimestre, name='por_anno_y_cuatrimestre'),
+    path('juntar_materias', views.juntar_materias, name='juntar_materias'),
     path('administrar_materia/<int:materia_id>/<int:anno>/<str:cuatrimestre>',
          views.administrar_materia, name='administrar_materia'),
     path('exportar_informacion/<int:anno>/<str:cuatrimestre>',
@@ -26,4 +26,5 @@ urlpatterns = [
     path('borrar_turno/<int:turno_id>', views.borrar_turno, name='borrar_turno'),
     path('cambiar_turno/<int:turno_id>', views.cambiar_turno, name='cambiar_turno'),
     path('borrar_horario/<int:horario_id>', views.borrar_horario, name='borrar_horario'),
+    path('<str:anno_cuat>', views.por_anno_y_cuatrimestre, name='por_anno_y_cuatrimestre'),
 ]
