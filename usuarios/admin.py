2dc64ce46b15bf2bd82d5f3ec30978c389caab29
Matías Graña <matias.alejo@gmail.com>
2019-08-31 20:32:39 -0300

    agrego AdminDeUsuario

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import Group
 import django.contrib.auth.admin
 
 from .models import Usuario, Grupo
+from .views import FormularioCreacionUsuario, FormularioCambioUsuario
 
 admin.site.unregister([Group])
-admin.site.register([Usuario, Grupo])
+admin.site.register([Grupo])
+
+
+
+class AdminDeUsuario(django.contrib.auth.admin.UserAdmin):
+    add_form = FormularioCreacionUsuario
+    form = FormularioCambioUsuario
+    model = Usuario
+    list_display = ['username']
+
+admin.site.register(Usuario, AdminDeUsuario)
