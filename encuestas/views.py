7f152df9fc24bb9547e8c2cff9bd89adf3417c93
Matías Graña <matias.alejo@gmail.com>
2019-10-28 01:01:00 -0300

    separo con bordes las tablas

--- encuestas/views.py
+++ encuestas/views.py
@@ -289,9 +289,11 @@ def encuestas_de_un_docente(request, docente_id, anno, cuatrimestre):
                                                       fecha_encuesta=fecha).cargas
                             )
                     for fecha in sorted(fechas, reverse=True)}
+    otros_datos = OtrosDatos.objects.get(docente=docente, anno=anno, cuatrimestre__contains=cuatrimestre)
 
     return render(request, 'encuestas/encuestas_de_un_docente.html',
                   {'anno': anno, 'cuatrimestre': Cuatrimestres[cuatrimestre],
                    'docente': docente,
                    'preferencias': preferencias,
+                   'otros_datos': otros_datos,
                    })
