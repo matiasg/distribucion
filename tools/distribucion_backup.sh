7bec3028e874417ce6049323b604990e92b4d30a
Matías Graña <matias.alejo@gmail.com>
2019-10-29 23:17:12 -0300

    remove old files

--- tools/distribucion_backup.sh
+++ tools/distribucion_backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "generating backup"
 DIR=$(dirname $0)
 docker run --rm -v distribucion_pgdata:/source:ro busybox tar -czC /source . > ${DIR}/data_backup_$(date "+%F_%T").tar.gz
+
+echo "keeping only last 30 files"
+ls ${DIR} | sort | head -n-30 | xargs rm
