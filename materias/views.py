0fe93633b5039ef26c4383a7fd0877e56f3f5dcf
Matías Graña <matias.alejo@gmail.com>
2019-12-08 09:17:17 -0300

    paso el test

--- materias/views.py
+++ materias/views.py
@@ -346,13 +346,14 @@ def cargas_docentes_anuales(request, anno):
         docentes_cargos_ordenados = {tipo: sorted(por_tipo_cargo[tipo], key=lambda dc: strxfrm(dc[0].apellido_nombre))
                                      for tipo in TipoDocentes}
 
-        def comentario_y_cargas_declaradas(doc_cargo):
-            otros_datos = OtrosDatos.objects.filter(anno=anno, docente=doc_cargo[0]).order_by('fecha_encuesta')
+        def comentarios_y_cargas_declaradas(doc_cargo):
+            otros_datos = OtrosDatos.objects.filter(anno=anno, docente=doc_cargo[0]).order_by('-fecha_encuesta')
             if otros_datos:
-                ultimos_datos = otros_datos.last()
+                ultimos_datos = otros_datos.first()
                 asignadas_al_periodo = sum(contados[Cuatrimestres[cuat]][doc_cargo]
                                            for cuat in ultimos_datos.cuatrimestre)
-                return [ultimos_datos.comentario, ultimos_datos.cargas_declaradas,
+                return [[od.comentario for od in otros_datos.all()],
+                        ultimos_datos.cargas_declaradas,
                         asignadas_al_periodo, ultimos_datos.cuatrimestre]
             else:
                 return ['', None, None, None]
@@ -365,7 +366,7 @@ def cargas_docentes_anuales(request, anno):
         def asignadas_pedidas_declaradas_comentario(doc_cargo, tipo):
             asignadas_pedidas = [AsignadasPedidas(contados[cuat][doc_cargo], pedidas(doc_cargo[0], cuat, tipo))
                                  for cuat in (Cuatrimestres.V, Cuatrimestres.P, Cuatrimestres.S)]
-            return asignadas_pedidas + comentario_y_cargas_declaradas(doc_cargo)
+            return asignadas_pedidas + comentarios_y_cargas_declaradas(doc_cargo)
 
         cargas = {tipo: {doc_cargo: asignadas_pedidas_declaradas_comentario(doc_cargo, tipo)
                          for doc_cargo in docentes_cargos_ordenados[tipo]}
